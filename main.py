0ee4f9f639d5a6e16e21a452fb40c582e2e7fffc
Melvin Klein <melvin.klein@stackit.cloud>
2024-06-14 07:07:31 +0000

    Merged PR 53534: chore: remove folder structure change artifacts

--- main.py
+++
@@ -1,6 +0,0 @@
-from rag_core.main import app as perfect_rag_app, register_dependency_container
-
-from use_case_container import UseCaseContainer
-
-
-register_dependency_container(UseCaseContainer())
--- use_case_container.py
+++
@@ -1,9 +0,0 @@
-from dependency_injector.containers import copy, DeclarativeContainer
-from dependency_injector.providers import Singleton
-from rag_core.dependency_container import DependencyContainer
-
-from example_replacement_chat_chain import ExampleReplacementChatChain
-
-
-class UseCaseContainer(DeclarativeContainer):
-    chat_chain = Singleton(ExampleReplacementChatChain)

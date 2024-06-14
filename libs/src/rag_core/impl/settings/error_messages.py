a69db3aed5fd9f7327faffb4356bb563d5589255
Melvin Klein <melvin.klein@stackit.cloud>
2024-06-14 07:08:08 +0000

    chore: remove doctopus-specific error message

--- libs/src/rag_core/impl/settings/error_messages.py
+++ libs/src/rag_core/impl/settings/error_messages.py
@@ -19,7 +19,3 @@ class ErrorMessages(BaseSettings):
     no_or_empty_collection: str = Field(
         default="Es tut mir leid, aber es wurden keine Dokumente bereitgestellt, welche ich durchsuchen könnte."
     )
-    no_product_name_message: str = Field(
-        default="""Es wurde kein Produktname im chat-verlauf gefunden.
-        Ich habe derzeit Informationen über folgende Produkte:"""
-    )

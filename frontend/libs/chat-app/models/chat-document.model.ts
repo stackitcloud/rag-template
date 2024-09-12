28aef726438b9a07574ca19d44b78cd85b36c7d2
Melvin Klein <melvin.klein@stackit.cloud>
2024-09-12 12:54:31 +0000

    Merged PR 73933: fix: title of sources in frontend
    fix title of documents

--- frontend/libs/chat-app/models/chat-document.model.ts
+++ frontend/libs/chat-app/models/chat-document.model.ts
@@ -11,7 +11,7 @@ export interface ChatDocumentModel {
 export const mapToChatDocuments = (startIncrementId: number, documents: DocumentResponseModel[], messageId: string) => documents.map((doc, index) => ({
   index: startIncrementId + index,
   messageId: messageId,
-  name: (doc.metadata.find(meta => (meta.key === "document" || meta.key === "title"))?.value as string).replace(/^"+|"+$/g, '') || "",  // TODO: handle nested json-text values correctly
+  name: (doc.metadata.find(meta => (meta.key === "title" || meta.key === "document"))?.value as string).replace(/^"+|"+$/g, '') || "",  // TODO: handle nested json-text values correctly
   chunk: doc.content,
   url: (doc.metadata.find(meta => meta.key === "document_url")?.value as string).replace(/^"+|"+$/g, '') || ""  // TODO: handle nested json-text values correctly
 }) as ChatDocumentModel);

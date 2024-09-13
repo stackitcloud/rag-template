0995912cce17a12dce78fbf842c4c86ac7bddb8b
Manuel Hoffmann <manuel.hoffmann@stackit.cloud>
2024-09-13 08:27:26 +0000

    Merged PR 74163: feature(UI): group sources by title

--- frontend/libs/chat-app/ui/ChatDocumentContainer.vue
+++ frontend/libs/chat-app/ui/ChatDocumentContainer.vue
@@ -1,44 +1,52 @@
-<script lang="ts"
-        setup>
-        import ChatDocumentItem from './ChatDocumentItem.vue';
-        import { computed } from "vue";
-        import { useI18n } from 'vue-i18n';
-        import { ChatDocumentItemModel } from '../models/chat-document-item.model';
-        import { SideBarContainer } from '@shared/ui';
-        import { ChatDocumentModel } from '../models/chat-document.model';
+<script lang="ts" setup>
+import ChatDocumentItem from './ChatDocumentItem.vue';
+import ChatDocumentGroup from './ChatDocumentGroup.vue';
+import { computed } from "vue";
+import { useI18n } from 'vue-i18n';
+import { isNotEmpty } from '@shared/utils';
+import { ChatDocumentItemModel } from '../models/chat-document-item.model';
+import { SideBarContainer } from '@shared/ui';
+import { ChatDocumentModel } from '../models/chat-document.model';
 
-        const { t } = useI18n()
+const { t } = useI18n()
 
-        const props = defineProps<{
-            documents: Array<ChatDocumentModel>
-        }>()
+const props = defineProps<{
+    documents: Array<ChatDocumentModel>
+}>()
 
-        const mapToDocumentItem = (document: ChatDocumentModel): ChatDocumentItemModel => ({
-            index: document.index,
-            source: document.url,
-            text: document.chunk,
-            title: document.name,
-            anchorId: document.messageId
-        });
+const mapToDocumentItem = (document: ChatDocumentModel): ChatDocumentItemModel => ({
+    index: document.index,
+    source: document.url,
+    text: document.chunk,
+    title: document.name,
+    anchorId: document.messageId
+});
 
-        const documents = computed((): ChatDocumentItemModel[] => {
-            return props.documents.map((message) => mapToDocumentItem(message));
-        });
+const groupedDocuments = computed((): ChatDocumentItemModel[] => {
+    const documents = props.documents.map((message) => mapToDocumentItem(message));
+    const grouped = documents.reduce(
+        (reduced: any, element: ChatDocumentItemModel) => {
+            reduced[element.title] = reduced[element.title] || [];
+            reduced[element.title].push(element);
+            return reduced;
+        }, Object.create(null));
+    return grouped;
+});
 </script>
 <template>
     <SideBarContainer :header="t('chat.sources')">
-        <div v-if="documents.length > 0">
-            <ChatDocumentItem v-for="document in documents"
-                              :key="document.index"
-                              :id="document.index"
-                              :title="document.title"
-                              :text="document.text"
-                              :source="document.source"
-                              :anchorId="document.anchorId"
-                              :index="document.index" />
+        <div v-if="isNotEmpty(groupedDocuments)">
+            <ChatDocumentGroup v-for="(documents, title, index) in groupedDocuments" :key="index"
+                :title="title" :source="documents[0].source">
+                <ChatDocumentItem v-for="document in documents" :key="document.index" :id="document.index"
+                    :title="document.title" :text="document.text" :source="document.source"
+                    :anchorId="document.anchorId" :index="document.index" />
+            </ChatDocumentGroup>
         </div>
         <div v-else>
-            <!--To be defined-->
+            <div class="flex items-center justify-center">
+                <p class="text-xs px-auto py-12 text-gray-500 text-center word-break">{{ t('chat.noSourcesHint') }}</p>
+            </div>
         </div>
     </SideBarContainer>
 </template>
---
+++ frontend/libs/chat-app/ui/ChatDocumentGroup.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ArrowTopRightOnSquareIcon, DocumentIcon } from "@heroicons/vue/24/outline";
+import { ChatDocumentGroupModel } from "../models/chat-document-group.model";
+
+const data = defineProps<ChatDocumentGroupModel>();
+</script>
+<template>
+    <div class="border-b py-2 md:py-4 px-4 flex gap-4">
+        <!-- <div class="flex items-center">
+            <div class="rounded-full p-4 bg-base-200">
+                <DocumentIcon class="w-4 h-4 text-primary" />
+            </div>
+        </div> -->
+        <div class="flex-1 break-words text-ellipsis overflow-hidden flex flex-col">
+            <h4 class="font-medium flex items-center gap-2 mb-1">
+                {{ data.title }}
+
+                <a class="text-sm text-primary cursor-pointer hover:opacity-75" :href="data.source" target="_blank">
+                    <ArrowTopRightOnSquareIcon class="h-4 w-4" />
+                </a>
+            </h4>
+            <slot></slot>
+        </div>
+    </div>
+</template>

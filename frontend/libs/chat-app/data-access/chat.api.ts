83c4537397c3d270ba49b6c1fc0e9dd242730561
Melvin Klein <melvin.klein@stackit.cloud>
2024-09-16 07:49:12 +0000

    feat: makes basic.auth for not admin optional

--- frontend/libs/chat-app/data-access/chat.api.ts
+++ frontend/libs/chat-app/data-access/chat.api.ts
@@ -3,10 +3,12 @@ import { ChatRequestModel } from "../models/chat-request.model";
 import { ChatResponseModel } from "../models/chat-response.model";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-axios.defaults.auth = {
-    username: import.meta.env.VITE_AUTH_USERNAME,
-    password: import.meta.env.VITE_AUTH_PASSWORD
-};
+if (import.meta.env.VITE_CHAT_AUTH_ENABLED) {
+  axios.defaults.auth = {
+      username: import.meta.env.VITE_AUTH_USERNAME,
+      password: import.meta.env.VITE_AUTH_PASSWORD
+  };
+}
 
 export class ChatAPI {
     static async callInference(request: ChatRequestModel): Promise<ChatResponseModel> {

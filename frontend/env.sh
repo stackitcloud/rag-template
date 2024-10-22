f8c8204b533d7520d4e2532ec61959480056ff16
Youssef Sbai <youssef.sbai@stackit.cloud>
2024-10-22 12:33:36 +0000

    Merged PR 82665: fix(Frontend): removed an echo command from the env script
    removed an echo command from the env script

--- frontend/env.sh
+++ frontend/env.sh
@@ -4,7 +4,6 @@ for i in $(env | grep VITE_)
 do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2-)
-    echo $key=$value
     # sed All files
     # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
     # sed JS and CSS only

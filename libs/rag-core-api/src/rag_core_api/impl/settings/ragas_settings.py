f8ecf227fcc58fb02fa917cfebbd1751941c16af
Andreas Klos <andreas.klos@stackit.cloud>
2024-11-28 09:44:07 +0000

    refactor: ragas settings
    refactor: ragas settings

--- libs/rag-core-api/src/rag_core_api/impl/settings/ragas_settings.py
+++ libs/rag-core-api/src/rag_core_api/impl/settings/ragas_settings.py
@@ -13,12 +13,10 @@ class RagasSettings(BaseSettings):
         env_prefix = "RAGAS_"
         case_sensitive = False
 
-    is_debug: bool = Field(default=False)
     use_openai: bool = Field(default=True)
     model: str = Field(default="gpt-4o-mini")
     openai_api_key: str = Field(default="")
     timeout: int = Field(default=60)
-    prompt_language: str = Field(default="german")
     evaluation_dataset_name: str = Field(default="eval-data")  # TODO: add config param in deployment
     dataset_filename: str = Field(default="test_data.json")
     max_concurrency: int = Field(default=5)

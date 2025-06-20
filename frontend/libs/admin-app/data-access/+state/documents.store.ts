420f67b5ceaa780d42b2dd2e57a51a5b872a0fcd
Sebastian Heußer <sebastian.heusser@stackit.cloud>
2025-06-20 13:51:45 +0200

    fix: merge conflicts

--- frontend/libs/admin-app/data-access/+state/documents.store.ts
+++ frontend/libs/admin-app/data-access/+state/documents.store.ts
@@ -2,21 +2,18 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { DocumentModel } from "../../models/document.model.ts";
 import { ErrorType } from "../../models/error-type";
-import { UploadedDocument, mapToUploadDocument } from "../../models/uploaded-document.model";
+import {
+  UploadedDocument,
+  mapToUploadDocument,
+} from "../../models/uploaded-document.model";
 import { ConfluenceConfig, DocumentAPI, SitemapConfig } from "../document.api";
 
-export const useDocumentsStore = defineStore('chat', () => {
+export const useDocumentsStore = defineStore("chat", () => {
   const uploadedDocuments = ref<UploadedDocument[]>([]);
   const allDocuments = ref<DocumentModel[]>();
   const error = ref<ErrorType | null>(null);
   const isLoadingConfluence = ref(false);
   const isLoadingSitemap = ref(false);
-
-
-  const uploadedDocuments = ref<UploadedDocument[]>([]);
-  const allDocuments = ref<DocumentModel[]>();
-  const error = ref<ErrorType | null>(null);
-  const isLoadingConfluence = ref(false);
 
   function updateUploadedDocumentData(
     documentId: string,
@@ -69,68 +66,67 @@ export const useDocumentsStore = defineStore('chat', () => {
     }
   };
 
-    const loadConfluence = async (config: ConfluenceConfig) => {
-      isLoadingConfluence.value = true;
-      error.value = null;
-      try {
-        // provide confluence configuration from frontend
-        await DocumentAPI.loadConfluence(config);
-        await loadDocuments(); // Refresh the document list after uploading
-      } catch(err) {
-        if (err.response && err.response.status === 501) {
-          error.value = "confluence_not_configured";
-          console.error("Confluence loader is not configured.");
-        } else if (err.response && err.response.status === 423) {
-          error.value = "confluence_locked";
-          console.error("Confluence loader returned a warning.");
-        } else {
-          error.value = "confluence";
-          console.error(err);
-        }
-      } finally {
-        isLoadingConfluence.value = false;
+  const loadConfluence = async (config: ConfluenceConfig) => {
+    isLoadingConfluence.value = true;
+    error.value = null;
+    try {
+      // provide confluence configuration from frontend
+      await DocumentAPI.loadConfluence(config);
+      await loadDocuments(); // Refresh the document list after uploading
+    } catch (err) {
+      if (err.response && err.response.status === 501) {
+        error.value = "confluence_not_configured";
+        console.error("Confluence loader is not configured.");
+      } else if (err.response && err.response.status === 423) {
+        error.value = "confluence_locked";
+        console.error("Confluence loader returned a warning.");
+      } else {
+        error.value = "confluence";
+        console.error(err);
       }
     } finally {
       isLoadingConfluence.value = false;
     }
   };
 
-    const loadSitemap = async (config: SitemapConfig) => {
-      isLoadingSitemap.value = true;
-      error.value = null;
-      try {
-        // provide sitemap configuration from frontend
-        await DocumentAPI.loadSitemap(config);
-        await loadDocuments(); // Refresh the document list after uploading
-      } catch(err) {
-        if (err.response && err.response.status === 501) {
-          error.value = "sitemap_not_configured";
-          console.error("Sitemap loader is not configured.");
-        } else if (err.response && err.response.status === 423) {
-          error.value = "sitemap_locked";
-          console.error("Sitemap loader returned a warning.");
-        } else {
-          error.value = "sitemap";
-          console.error(err);
-        }
-      } finally {
-        isLoadingSitemap.value = false;
+  const loadSitemap = async (config: SitemapConfig) => {
+    isLoadingSitemap.value = true;
+    error.value = null;
+    try {
+      // provide sitemap configuration from frontend
+      await DocumentAPI.loadSitemap(config);
+      await loadDocuments(); // Refresh the document list after uploading
+    } catch (err) {
+      if (err.response && err.response.status === 501) {
+        error.value = "sitemap_not_configured";
+        console.error("Sitemap loader is not configured.");
+      } else if (err.response && err.response.status === 423) {
+        error.value = "sitemap_locked";
+        console.error("Sitemap loader returned a warning.");
+      } else {
+        error.value = "sitemap";
+        console.error(err);
       }
-    };
+    } finally {
+      isLoadingSitemap.value = false;
+    }
+  };
 
-    const uploadDocuments = async (files: File[]) => {
-        try {
-            const uploads = files.map(uploadDocument);
-            await Promise.all(uploads);
-            await new Promise((resolve) => setTimeout(resolve, 250)); // short delay for the user to see the progress
-            await loadDocuments();
-        } catch(err) {
-            error.value = "upload";
-            console.error(err);
-        } finally {
-            uploadedDocuments.value = uploadedDocuments.value.filter(o => o.isFailed);
-        }
-    };
+  const uploadDocuments = async (files: File[]) => {
+    try {
+      const uploads = files.map(uploadDocument);
+      await Promise.all(uploads);
+      await new Promise((resolve) => setTimeout(resolve, 250)); // short delay for the user to see the progress
+      await loadDocuments();
+    } catch (err) {
+      error.value = "upload";
+      console.error(err);
+    } finally {
+      uploadedDocuments.value = uploadedDocuments.value.filter(
+        (o) => o.isFailed,
+      );
+    }
+  };
 
   const deleteDocument = async (documentId: string) => {
     try {
@@ -148,7 +144,16 @@ export const useDocumentsStore = defineStore('chat', () => {
     );
   };
 
-
-  return {removeUploadedDocument, uploadDocuments, loadDocuments, deleteDocument, loadConfluence, loadSitemap, allDocuments, uploadedDocuments, error, isLoadingSitemap};
-
+  return {
+    removeUploadedDocument,
+    uploadDocuments,
+    loadDocuments,
+    deleteDocument,
+    loadConfluence,
+    loadSitemap,
+    allDocuments,
+    uploadedDocuments,
+    error,
+    isLoadingSitemap,
+  };
 });

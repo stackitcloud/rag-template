ca31b312ccffdca77e8fc5932151fe8316338495
Andreas Klos <andreas.klos@stackit.cloud>
2025-07-15 13:42:30 +0200

    fix: remove unnecessary space in dns_name assignment in dns.tf

--- infrastructure/terraform/dns.tf
+++ infrastructure/terraform/dns.tf
@@ -1,7 +1,7 @@
 resource "stackit_dns_zone" "rag_zone" {
   project_id = var.project_id
   name       = "${var.name_prefix}-zone"
-  dns_name   =  var.dns_name
+  dns_name   = var.dns_name
 }
 
 output "dns_nameservers" {

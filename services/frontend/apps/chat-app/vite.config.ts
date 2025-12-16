922b80d39a637025269f57f7edf92366bd17bad4
Andreas Klos <andreas.klos@stackit.cloud>
2025-12-16 12:04:41 +0100

    chore(deps): Major version updates of the frontends. (#137)

--- services/frontend/apps/chat-app/vite.config.ts
+++ services/frontend/apps/chat-app/vite.config.ts
@@ -1,34 +1,39 @@
 /// <reference types="vitest" />
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { fileURLToPath } from 'url';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
+
 const CWD = process.cwd();
 
 export default defineConfig(({ mode }) => {
   const envs = loadEnv(mode, CWD);
   return {
     ...envs,
+    root: fileURLToPath(new URL('./', import.meta.url)),
     cacheDir: '../../node_modules/.vite/chat-app',
     server: {
       port: 4200,
       host: 'localhost',
-
       fs: {
-        allow: [
-          '../../libs/i18n'
-        ],
+        allow: ['../../libs/i18n'],
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: fileURLToPath(new URL('./index.html', import.meta.url)),
       },
     },
     plugins: [
       vue(),
       nxViteTsPaths(),
       VueI18nPlugin({
-        include: '../../libs/chat-app/i18n/locales/**',
+        include: '../../libs/i18n/chat/**/*.json',
       }),
+      tailwindcss(),
     ],
-
     resolve: {
       alias: {
         '@chat-app/i18n': fileURLToPath(
@@ -36,8 +41,8 @@ export default defineConfig(({ mode }) => {
         ),
         '@shared/style': fileURLToPath(
           new URL('../../libs/shared/global.css', import.meta.url)
-        )
+        ),
       },
-    }
-  }
+    },
+  };
 });
